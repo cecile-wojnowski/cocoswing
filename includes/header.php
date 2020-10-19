12a72037c7ba99aef0136e9c7642f62ffe76039c
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-19 11:35:24 +0200

    Nav header

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<header>
+  <nav class="nav_header">
+    <ul>
+      <li><a href="#"> Home </a></li>
+      <li><a href="#"> Les cocos </a></li>
+      <li><a href="#"> Cours & stages </a></li>
+      <li><a href="#"> Evènements </a></li>
+      <li><a href="#"> Animations </a></li>
+      <li><a href="#"> Culture Swing </a></li>
+      <li><a href="#"> Mon compte </a></li>
+    </ul>
+  </nav>
+</header>
