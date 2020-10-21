036890462ee72e04c2dfed0efd1f66203c7f256c
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-21 14:28:36 +0200

    HTML + Header

---
+++ presentation.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e9a44ab6cf.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/footer.css">
+  </head>
+
+  <body>
+    <?php include("includes/header.php"); ?>
+    <div class="img_header">
+      <section class="img_index"></section>
+    </div>
+
+    <main>
+
+    </main>
+
+    <?php include("includes/footer.php"); ?>
+  </body>
+</html>
