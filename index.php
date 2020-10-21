79a57b34d9b33a253856c42e3c44740a7120d720
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-21 13:36:53 +0200

    Ajout icones + img temporaire events fb

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e9a44ab6cf.js" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/normalize.css">
@@ -19,7 +20,7 @@
     <main>
       <section class="img_index"></section>
 
-      <section class="text_index">
+      <section class="content_index">
         <h1 class="h1_index">
           T'as le Groove Coco. Coco t'as le Groove !
         </h1>
@@ -30,9 +31,17 @@
           Tu découvriras un monde de fête, de joies et de copineries. <br>
         </p>
 
-        <button type="button" name="button_inscription" class="button_inscription"> Inscriptions </button>
+        <button type="button" name="button_inscription" class="button_index"> Inscriptions </button>
+
+        <section class="actu_fb">
+          <img id="actu_fb" src="img/event_temporary" alt="need_to_be_replaced">
+        </section>
+
+        <button type="button" name="button_inscription" class="button_index"> Newsletter </button>
 
       </section>
+
+
 
     </main>
 
