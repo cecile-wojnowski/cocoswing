79692e4184aedf4b0620646a0f814d88c6a8f885
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-21 14:28:15 +0200

    Header ayant une image à la hauteur < img header index

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,13 @@
 <header>
   <nav class="nav_header">
     <ul>
-      <li><a href="#"> Home </a></li>
-      <li><a href="#"> Les cocos </a></li>
-      <li><a href="#"> Cours & stages </a></li>
-      <li><a href="#"> Evènements </a></li>
+      <li><a href="index.php"> Home </a></li>
+      <li><a href="presentation.php"> Les cocos </a></li>
+      <li><a href="cours.php"> Cours & stages </a></li>
+      <li><a href="evenements.php"> Evènements </a></li>
       <li><a href="#"> Animations </a></li>
       <li><a href="#"> Culture Swing </a></li>
-      <li><a href="#"> Mon compte </a></li>
+      <li><a href="mon_compte.php"> Mon compte </a></li>
     </ul>
   </nav>
 </header>
