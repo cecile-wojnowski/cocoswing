9fb138d6431184d4a1f955968134526981841655
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-21 15:42:13 +0200

    HTML

---
+++ inscription.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Créer un compte</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e9a44ab6cf.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/form_connexion.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/presentation.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/footer.css">
+  </head>
+
+  <body>
+    <?php include("includes/header.php"); ?>
+    <div class="img_header">
+      <section class="img_index"></section>
+    </div>
+
+    <main>
+      <h1> </h1>
+
+    </main>
+
+  </body>
+</html>
