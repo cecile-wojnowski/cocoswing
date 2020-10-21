7dfc1159cf80c60fb6a3ac90a8538c204b73e9ea
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-21 13:36:16 +0200

    HTML footer

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<footer>
+  <h2>COCO SWING MARSEILLE</h2>
+
+  <div class="container_footer">
+    <ul>
+      <li> Contact </li>
+      <li> Adhésion </li>
+    </ul>
+
+    <ul>
+      <li>Retrouvez-nous</li>
+      <li><i class="fab fa-facebook-square"></i> <i class="fab fa-instagram-square"></i> <i class="fab fa-youtube-square"></i></li>
+    </ul>
+  </div>
+</footer>
