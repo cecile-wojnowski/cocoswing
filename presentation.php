4b52a8aac2b115252325d59fa455783dc302b3b5
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-21 14:59:27 +0200

    Textes & titres

--- presentation.php
+++ presentation.php
@@ -8,6 +8,7 @@
     <script src="https://kit.fontawesome.com/e9a44ab6cf.js" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/presentation.css">
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/main.css">
@@ -25,9 +26,31 @@
         <h1> L'association </h1>
         <h2> Swing Coco, swing !</h2>
 
-        <p></p>
-        <p></p>
-        <p></p>
+        <p>
+          Au commencement, il y a une bande de copains et de copines qui se retrouvent autour d’une découverte commune :
+          le Lindy Hop.
+          A force de danser chez les uns les autres, de swinguer dehors, sur les places,
+          au bord de la mer, sur le Vieux-Port ou dans la rue, la lubie devient passion et la bande de potes s’agrandit.
+          Il nous fallait un vrai lieu pour apprendre et partager, il nous fallait des endroits pour faire la fête,
+          pour inviter des musiciens et étendre très loin la culture du swing, les gammes folles du jazz
+          et toutes les amitiés qui s’ensuivent.
+        </p>
+        <p>
+          Et si on faisait une association ? Et si cette association faisait aussi école ? Oh pichou que oui !
+          Alors que l’idée germe, voici que Coco Velten nous ouvre ses portes.
+          L’accueil dans ce centre socio-culturel mis en place par Yes We Camp nous fait nous sentir là où nous voulions être :
+          un lieu où le préfixe co-déploie tous ses sens. Copineries, commun, coopération, « t’as le look Coco ».
+          Nous y proposons alors des initiations à prix libre, puis des vrais cours, puis des soirées de fête.
+          Si bien qu’à un moment il va de soi pour nous de nous appeler Coco Swing Marseille.
+        </p>
+        <p>
+          Dès lors, nos cours essaiment par-ci par-là, la communauté grandit,
+          les propositions de festivals et de soirées se font de plus en plus créatives.
+          Coco Swing Marseille se donne l’ambition de mettre en acte toutes les beautés des danses
+          et de la culture swing à Marseille. Un espace de joie, de subtilité du geste et de la musique,
+          de mise en lumière de la culture africaine-américaine et surtout, encore une fois, d’amitiés.
+          Bienvenue parmi celles et ceux que l’on appelle Coco.
+        </p>
       </section>
 
       <section id="presentation_profs">
