315960c9e15eeac8bc725410058676013dbd5d3e
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-28 19:11:20 +0100

    placement des éléments avec grid

--- adhesion.php
+++ adhesion.php
@@ -29,28 +29,33 @@
       <h2 class="h2_compte">Choix de cours annuels</h2>
 
       <form class="form_adhesion" action="adhesion.php" method="post">
-        <div class="textes_radio">
+
           <p id="p_cours">Choisir un cours</p>
           <p id="p_nombre_cours">Nombre de cours hebdomadaires</p>
+
+        <div class="blue_radio" id="blue_radio1">
+            <input type="radio" id="solo" name="solo" value="solo">
+            <label for="solo">Solo</label>
         </div>
-        <div class="blue_radio">
-          <input type="radio" id="solo" name="solo" value="solo">
-          <label for="solo">Solo</label>
+        <div class="blue_radio" id="blue_radio2">
+            <input type="radio" id="once" name="once" value="once">
+            <label for="once">1x par semaine</label>
+          </div>
 
-          <input type="radio" id="once" name="once" value="once">
-          <label for="once">1x par semaine</label>
-        </div>
+        <div class="orange_radio" id="orange_radio1">
 
-        <div class="orange_radio">
-          <input type="radio" id="lindy" name="lindy" value="lindy">
-          <label for="lindy">Lindy</label>
+            <input type="radio" id="lindy" name="lindy" value="lindy">
+            <label for="lindy">Lindy</label>
 
+          </div>
+        <div class="orange_radio" id="orange_radio2">
           <input type="radio" id="twice" name="twice" value="twice">
           <label for="twice">2x par semaine</label>
+
         </div>
 
-        <section class="bottom_form_adhesion">
-          <div>
+
+          <div id="reductions_radio">
             <p> Réductions </p>
             <span>
               <input type="radio" id="student_price" name="student_price" value="Tarif étudiant">
@@ -63,7 +68,7 @@
             </span>
           </div>
 
-          <div class="">
+          <div id="adherent_radio">
             <p>Êtes-vous adhérent ? </p>
             <span>
               <input type="radio" id="adherent_true" name="adherent_true" value="Oui">
@@ -75,7 +80,7 @@
             </span>
           </div>
 
-          <div class="">
+          <div id="paiement_radio">
             <p> Payer en plusieurs fois </p>
             <span>
               <input type="radio" id="paiement_unique" name="paiement_unique" value="1x">
@@ -86,9 +91,9 @@
               <label for="adherent_false">En 3x sans frais</label>
             </span>
           </div>
-        </section>
 
-        <button type="button" name="form_inscription" class="button_pages"> Adhérer </button>
+
+        <button type="button" name="form_inscription" class="button_pages" id="btn_adhesion"> Adhérer </button>
       </form>
 
     </main>
