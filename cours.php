b720648533f50a14e415a073b600049a455bd3c8
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-28 13:32:07 +0100

    CSS bouton devant changer d'apparence au clic

---
+++ cours.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Qui sommes-nous</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e9a44ab6cf.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+    <link rel="stylesheet" href="css/animation.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/presentation.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/footer.css">
+  </head>
+
+  <body>
+    <?php include("includes/header.php"); ?>
+    <div class="img_header">
+      <section class="img_index"></section>
+    </div>
+
+    <main>
+      <h1> Cours & stages </h1>
+
+      <button type="button" name="button" class="btn_cours"> Cours réguliers </button>
+    </main>
+
+  </body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="js/animate.js" charset="utf-8"></script>
+</html>
--- js/animate.js
+++ js/animate.js
@@ -1,4 +1,9 @@
+// Page presentation.php
 // Apparition/disparition en fondu des textes de présentation des profs
 $(".img_profs").click(function() {
   $( ".articles_profs" ).toggle( "fade" );
 });
+
+// Page cours.php
+// Effet d'explosition lors du clic sur le bouton
+// 0U effet de transition entre deux éléments : un bouton et une liste de liens
