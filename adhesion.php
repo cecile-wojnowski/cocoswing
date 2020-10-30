d2b8bbcd9787bb8323170a4ca28934aa1a5bf279
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-30 16:56:02 +0100

    Changement de couleur au clic avec JQuery

--- adhesion.php
+++ adhesion.php
@@ -100,4 +100,7 @@
     </main>
     <?php include("includes/footer.php"); ?>
   </body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="js/animate.js" charset="utf-8"></script>
 </html>
--- js/animate.js
+++ js/animate.js
@@ -7,3 +7,20 @@ $(".img_profs").click(function() {
 // Page cours.php
 // Effet d'explosition lors du clic sur le bouton
 // 0U effet de transition entre deux éléments : un bouton et une liste de liens
+
+
+// Page adhesion.php
+// Changement de couleur lors du clic sur l'une des cases
+$("#blue_radio1").click(function(){
+    $('#blue_radio1').css('background-color', '#044D59');
+});
+$("#blue_radio2").click(function(){
+    $('#blue_radio2').css('background-color', '#044D59');
+});
+
+$("#orange_radio1").click(function(){
+    $('#orange_radio1').css('background-color', '#945010');
+});
+$("#orange_radio2").click(function(){
+    $('#orange_radio2').css('background-color', '#945010');
+});
