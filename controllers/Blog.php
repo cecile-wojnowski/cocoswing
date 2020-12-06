aaf567e13d799ecc2c3bd773d2541161bb60191f
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-12-06 18:22:34 +0100

    views' blog

--- controllers/Blog.php
+++ controllers/Blog.php
@@ -1,29 +1,30 @@
 <?php
 class Blog extends Controller{
   public function index(){
-
-  }
-
-  public function createArticle(){
-
+    // website / blog.php
   }
 
   public function getArticle(){
-
+    // website / article.php
   }
-  public function updateArticle(){
 
+  public function createArticle(){
+    // admin / manage_blog.php
+  }
+
+  public function updateArticle(){
+    // admin / manage_blog.php
   }
   public function deleteArticle(){
-
+    // admin / manage_blog.php
   }
   public function createCategory(){
-
+    // admin / manage_blog.php
   }
   public function updateCategory(){
-
+    // admin / manage_blog.php
   }
   public function deleteCategory(){
-
+    // admin / manage_blog.php
   }
 } ?>
