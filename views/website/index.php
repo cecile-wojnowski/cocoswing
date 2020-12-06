25175938d149c4e1523ba98312574840fed6a7ec
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-12-06 18:57:32 +0100

    Superposition nav & img dans header index

--- views/website/index.php
+++ views/website/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Accueil</title>
+    <title>Cocoswing | Accueil</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
