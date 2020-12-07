82a60bc4e22553e85103689d759efab7840cdbc8
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-12-07 10:39:59 +0100

    rename files + render admin

--- controllers/Admin.php
+++ controllers/Admin.php
@@ -6,35 +6,40 @@ class Admin extends Controller{
   public function manageCourseRequest(){
     // Gérer les demandes d'inscription aux cours
 
-    // manage_requests.php
+    $this->render("admin/gestion-demandes");
   }
   public function manageSubscriptions(){
     // Gérer les formules
 
     // manage_subscriptions.php
+    $this->render("admin/gestion-formules");
   }
 
   public function checkDocument(){
     // Vérifier les documents envoyés par les utilisateurs
 
     // manage_documents.php
+    $this->render("admin/gestion-documents");
   }
 
   public function getUsers(){
     // Affiche la liste des membres inscrits
 
     // manage_users.php
+    $this->render("admin/gestion-membres");
   }
 
   public function manageRights(){
     // Gérer les droits des utilisateurs
 
     // manage_users.php
+    $this->render("admin/gestion-membres");
   }
 
   public function banUser(){
     // Bannir un utilisateur en l'empêchant de se connecter
 
     // manage_users.php
+    $this->render("admin/gestion-membres");
   }
 } ?>
