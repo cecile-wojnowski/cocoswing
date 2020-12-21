b1882818cfe0370c521bc3f519803ff82c030010
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2020-12-21 20:37:19 +0100

    profil_nav's file moved

---
+++ views/members/profil_nav.php
@@ -0,0 +1,7 @@
+<ul class="liste_compte">
+  <li><a class="a_compte" href="mon_profil.php"> Mes informations </a></li>
+  <li><a class="a_compte" href="adhesion.php"> S'inscrire à un cours </a></li>
+  <li><a class="a_compte" href="planning.php"> Voir le planning </a></li>
+  <li><a class="a_compte" href="mes_cours.php"> Mes demandes en attente </a></li>
+  <li><a class="a_compte" href="historique_paiements.php"> Mes paiements </a></li>
+</ul>
