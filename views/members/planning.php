0d8173a6aaa307c557ab9e214c8a9e777b15d97e
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2020-12-23 15:48:37 +0100

    CSS planning

--- views/members/planning.php
+++ views/members/planning.php
@@ -2,87 +2,97 @@
 
 <h2 class="h2_compte">Agenda des cours</h2>
 
-<table>
-	<thead>
-		<tr>
-			<th> </th>
-				<th scope="col"> Lundi </th>
-				<th scope="col"> Mardi </th>
-				<th scope="col"> Mercredi </th>
-				<th scope="col"> Jeudi </th>
+<div class="row">
+	<div class="col s7 m7 offset-m2">
+		<table class="centered">
+			<thead>
+				<tr>
+					<th> </th>
+					<th scope="col"> Lundi </th>
+					<th scope="col"> Mardi </th>
+					<th scope="col"> Mercredi </th>
+					<th scope="col"> Jeudi </th>
+				</tr>
+			</thead>
 
-		</tr>
-	</thead>
+			<tbody>
+				<tr>
+					<th scope="row"> 13h </th>
+					<td> </td>
+					<td> </td>
+					<td> </td>
+					<td> </td>
+				</tr>
+				<tr>
+					<th scope="row"> 14h </th>
+						<td>  </td>
+						<td> </td>
+						<td>  </td>
+						<td>  </td>
+				</tr>
+				<tr>
+					<th scope="row"> 15h </th>
+		      <td>  </td>
+		      <td> </td>
+		      <td>  </td>
+		      <td>  </td>
 
-	<tbody>
-		<tr>
-			<th scope="row"> 9h </th>
-				<td>  </td>
-				<td>  </td>
-				<td>  </td>
-				<td>  </td>
-		</tr>
-		<tr>
-			<th scope="row"> 10h </th>
-				<td>  </td>
-				<td> </td>
-				<td>  </td>
-        <td>  </td>
+				</tr>
+				<tr>
+					<th scope="row"> 16h </th>
+		      <td>  </td>
+		      <td> </td>
+		      <td>  </td>
+		      <td>  </td>
 
-		</tr>
-		<tr>
-			<th scope="row"> 11h </th>
-				<td> </td>
-				<td></td>
-				<td>  </td>
-        <td>  </td>
-		</tr>
-		<tr>
-			<th scope="row"> 12h </th>
-				<td class="pause_dejeuner" rowspan="2" colspan="4">  </td>
-		</tr>
-		<tr>
-			<th scope="row"> 13h </th>
-		</tr>
-		<tr>
-			<th scope="row"> 14h </th>
-				<td>  </td>
-				<td> </td>
-				<td>  </td>
-				<td>  </td>
-		</tr>
-		<tr>
-			<th scope="row"> 15h </th>
-      <td>  </td>
-      <td> </td>
-      <td>  </td>
-      <td>  </td>
+				</tr>
+				<tr>
+					<th scope="row"> 17h </th>
+		      <td>  </td>
+		      <td> </td>
+		      <td>  </td>
+		      <td>  </td>
 
-		</tr>
-		<tr>
-			<th scope="row"> 16h </th>
-      <td>  </td>
-      <td> </td>
-      <td>  </td>
-      <td>  </td>
+				</tr>
+				<tr>
+					<th scope="row"> 18h </th>
+		      <td>  </td>
+		      <td>  </td>
+		      <td>  </td>
+		      <td>  </td>
+				</tr>
 
-		</tr>
-		<tr>
-			<th scope="row"> 17h </th>
-      <td>  </td>
-      <td> </td>
-      <td>  </td>
-      <td>  </td>
+				<tr>
+					<th scope="row"> 19h </th>
+		      <td>  </td>
+		      <td>  </td>
+		      <td>  </td>
+		      <td>  </td>
+				</tr>
 
-		</tr>
-		<tr>
-			<th scope="row"> 18h </th>
-      <td>  </td>
-      <td> </td>
-      <td>  </td>
-      <td>  </td>
-
-		</tr>
-	</tbody>
-</table>
+				<tr>
+					<th scope="row"> 20h </th>
+		      <td>  </td>
+		      <td>  </td>
+		      <td>  </td>
+		      <td>  </td>
+				</tr>
+				<tr>
+					<th scope="row"> 21h </th>
+		      <td>  </td>
+		      <td>  </td>
+		      <td>  </td>
+		      <td>  </td>
+				</tr>
+				<tr>
+					<th scope="row"> 22h </th>
+		      <td>  </td>
+		      <td>  </td>
+		      <td>  </td>
+		      <td>  </td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
 <a href="#"> Ajouter un cours</a>
