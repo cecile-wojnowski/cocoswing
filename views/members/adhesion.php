c5eecaf1556981a2ceebf7eae60512f250cc4719
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2020-12-25 17:06:18 +0100

    Form with inputs radio using Materialize

--- views/members/adhesion.php
+++ views/members/adhesion.php
@@ -3,73 +3,86 @@
 <h2 class="h2_compte">Choix de cours annuels</h2>
 <div class="row">
 
-  <form class="form_adhesion" method="post">
-    <div class="col m8 s8 offset-m2">
-      <p class="center-align" id="p_cours">Choisir un cours</p>
+  <form action="#" method="post" class="center-align">
+    <p class="center-align p_cours">Choisir un cours</p>
+    <p>
+      <label>
+        <input name="group1" value="1solo" type="radio" checked />
+        <span>1 cours de Solo</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input name="group1" value="1lindy" type="radio" />
+        <span>1 cours de Lindy Hop</span>
+      </label>
+    </p>
+    <p class="center-align p_cours">Autres choix</p>
+    <p>
+      <label>
+        <input name="group1" value="1lindy_1solo" type="radio"  />
+        <span> 1 cours de Lindy + 1 cours de Solo</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input name="group1" value="2lindy" type="radio"  />
+        <span> 2 cours de Lindy</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input name="group1" type="radio" disabled="disabled" />
+        <span> 2 cours de Lindy + 1 cours de solo (disponible bientôt)</span>
+      </label>
+    </p>
 
-      <div class="blue_radio hidden">
-          <input type="radio" id="once" name="once" value="once">
-          <label for="once">1x par semaine</label>
-        </div>
+    <p class="center-align p_cours">Réductions</p>
+    <p>
+      <label>
+        <input name="group2" value="etudiant" type="radio"  />
+        <span> Tarif étudiant </span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input name="group2" value="rsa" type="radio"  />
+        <span> Bénéficiaire du RSA</span>
+      </label>
+    </p>
 
-        <div class="orange_radio hidden">
-          <input type="radio" id="twice" name="twice" value="twice">
-          <label for="twice">2x par semaine</label>
-        </div>
+    <p class="center-align p_cours">Etes-vous adhérent ?</p>
+    <p>
+      <label>
+        <input name="group3" value="etudiant" type="radio"  />
+        <span> Oui </span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input name="group3" value="rsa" type="radio"  />
+        <span> Non </span>
+      </label>
+    </p>
 
-      <div class="blue_radio col m3 offset-m2">
-          <input type="radio" id="solo" name="solo" value="solo">
-          <label for="solo">Solo</label>
-      </div>
+    <p class="center-align p_cours">Payer en plusieurs fois</p>
+    <p>
+      <label>
+        <input name="group3" value="1x" type="radio"  />
+        <span> En 1x </span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input name="group3" value="3x" type="radio"  />
+        <span> En 3x sans frais </span>
+      </label>
+    </p>
 
-      <div class="orange_radio col m3 offset-m2">
-        <input type="radio" id="lindy" name="lindy" value="lindy">
-        <label for="lindy">Lindy</label>
-      </div>
-    </div>
+    <button type="button" name="" class="button_pages" id="btn_adhesion">
+      <a href="#modal_justificatif" rel="modal:open"> Adhérer </a>
+    </button>
 
-    <div class="col m8 s8 offset-m2">
-      <div id="reductions_radio">
-        <p> Réductions </p>
-        <span>
-          <input type="radio" id="student_price" name="student_price" value="Tarif étudiant">
-          <label for="student_price"> Tarif étudiant </label>
-        </span>
-
-        <span>
-          <input type="radio" id="rsa" name="rsa" value="Bénéficiaire du RSA">
-          <label for="rsa">Bénéficiaire du RSA</label>
-        </span>
-      </div>
-
-      <div id="adherent_radio">
-        <p>Êtes-vous adhérent ? </p>
-        <span>
-          <input type="radio" id="adherent_true" name="adherent_true" value="Oui">
-          <label for="adherent_true">Oui</label>
-        </span>
-        <span>
-          <input type="radio" id="adherent_false" name="adherent_false" value="Non">
-          <label for="adherent_false">Non</label>
-        </span>
-      </div>
-
-      <div id="paiement_radio">
-        <p> Payer en plusieurs fois </p>
-        <span>
-          <input type="radio" id="paiement_unique" name="paiement_unique" value="1x">
-          <label for="paiement_unique">En 1x</label>
-        </span>
-        <span>
-          <input type="radio" id="triple_paiement" name="triple_paiement" value="3x">
-          <label for="adherent_false">En 3x sans frais</label>
-        </span>
-      </div>
-
-      <button type="button" name="form_inscription" class="button_pages" id="btn_adhesion">
-        <a href="#modal_justificatif" rel="modal:open"> Adhérer </a>
-      </button>
-    </div>
   </form>
 </div>
 
