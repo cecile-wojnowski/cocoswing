d50da2ad8399656128e9dccd748bae0182d43bac
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2020-12-25 17:32:45 +0100

    logo rel (onglet)

--- views/default.php
+++ views/default.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="icon" type="image/png" href="../ressources/img/logo.png" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/e9a44ab6cf.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../ressources/css/adhesion.css">
