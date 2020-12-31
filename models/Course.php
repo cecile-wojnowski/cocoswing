d0e93c828ac781dff60789e86580a81ac35b93d3
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2020-12-31 17:17:44 +0100

    Mise en forme des données récupérées en bdd

--- models/Course.php
+++ models/Course.php
@@ -19,22 +19,23 @@ class Course extends Model{
   }
 
   public function recupererCours(){
-    $courses = $this->getAll();
+    $courses = $this->getAll(); // On récupère tous les cours stockés en base de données
 
     $course = [];
-
     foreach($courses as $data){
 
-      $day = $data['day'] ;
-      $type_dance = $data['type_dance'];
+      $day = $data['day'] ; // Stocker $data['day'] dans une variable permettra de l'assigner comme index du tableau $course
       $level = $data['level'];
 
       // On formate la date de début pour ne récupérer que l'heure
       $start_time = new Datetime($data['start_time']);
       $time_format= $start_time->format('H');
 
+      // Mise en forme des noms de danse
+      $type_dance = strtoupper($data['type_dance']); // On met en majuscule le nom des danses
+      $dance_name = str_replace("_", " ", $type_dance); // On remplace le _ de type_dance par un espace vide
 
-      $course[$day][$time_format] = $type_dance . $level;
+      $course[$day][$time_format] = $dance_name ." ". $level; // Donne par exemple : $course['lundi'][18] = SOLO 2
     }
 
     return $course;
