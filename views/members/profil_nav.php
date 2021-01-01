103c145955bd218537f5927505960e95d1db3b07
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2021-01-01 18:37:18 +0100

    correction chemin

--- views/members/profil_nav.php
+++ views/members/profil_nav.php
@@ -1,5 +1,5 @@
 <ul class="liste_compte">
-  <li><a class="a_compte" href="mon_profil"> Mes informations </a></li>
+  <li><a class="a_compte" href="monProfil"> Mes informations </a></li>
   <li><a class="a_compte" href="adhesion"> S'inscrire à un cours </a></li>
   <li><a class="a_compte" href="planning"> Voir le planning </a></li>
   <li><a class="a_compte" href="mes_cours"> Mes demandes en attente </a></li>
