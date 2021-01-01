530412e0907fdd639de826a4c6889401b953268a
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2021-01-01 18:08:50 +0100

    Choix préétablis du type de danse et des niveaux

--- views/members/planning.php
+++ views/members/planning.php
@@ -117,31 +117,29 @@ function afficherCours($day, $hour, $course) {
   <h1> Ajouter un cours </h1>
     <form class="p-5 add_course" action="addCourse"  method="post">
 			<div class="row">
-				<div class=" col s6 m6">
-					<input type="text" name="type_dance" placeholder="Nom du cours" required>
+				<div class="col s6 m6">
+				<select name="type_dance">
+					<option value="" disabled selected>Choisir le type de danse</option>
+					<option value="solo">Solo </option>
+					<option value="lindy_hop">Lindy Hop</option>
+				</select>
 				</div>
 
 				<div class="col s6 m6">
 				<select name="level">
 					<option value="" disabled selected>Choisir le niveau du cours</option>
-					<optgroup label="Solo">
-						<option value="solo_1">Solo 1</option>
-						<option value="solo_2">Solo 2</option>
-					</optgroup>
-					<optgroup label="Lindy Hop">
-						<option value="lindy_1">Lindy Hop 1</option>
-						<option value="lindy_2">Lindy Hop 2</option>
-						<option value="lindy_3">Lindy Hop 3</option>
-					</optgroup>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
 				</select>
 				</div>
 
 			<div class="row">
 				<div class=" col s6 m6">
-					<input type="text" name="start_time" placeholder="Heure de début" required>
+					<input type="time" name="start_time" placeholder="Heure de début" required>
 				</div>
 				<div class=" col s6 m6">
-					<input type="text" name="end_time" placeholder="Heure de fin" required>
+					<input type="time" name="end_time" placeholder="Heure de fin" required>
 				</div>
 			</div>
 			<div class="row">
