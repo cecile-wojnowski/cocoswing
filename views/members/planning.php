3b9a6594e840e16ce2c2dcf6429a29dfb4ea84ef
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2021-01-01 15:59:54 +0100

    table centered

--- views/members/planning.php
+++ views/members/planning.php
@@ -12,7 +12,7 @@ function afficherCours($day, $hour, $course) {
 		elseif($course[$day][$hour][0] == "LINDY HOP 2")
 			$classe = "green_course";
 
-		echo "<button class='$classe course'>". "<b>" . $course[$day][$hour][1] ." - ".$course[$day][$hour][2]. "</b> <br>" 
+		echo "<button class='$classe course'>". "<b>" . $course[$day][$hour][1] ." - ".$course[$day][$hour][2]. "</b> <br>"
 		. $course[$day][$hour][0] ."</button>";
 	}
 }
@@ -23,92 +23,109 @@ function afficherCours($day, $hour, $course) {
 <h2 class="h2_compte">Agenda des cours</h2>
 
 <div class="row">
-	<div class="col s7 m7 offset-m2">
-		<table class="centered">
-			<thead>
-				<tr>
-					<th scope="col"> Lundi </th>
-					<th scope="col"> Mardi </th>
-					<th scope="col"> Mercredi </th>
-					<th scope="col"> Jeudi </th>
-					<th scope="col"> Vendredi </th>
-				</tr>
-			</thead>
+	<table class="centered" id="planning">
+		<thead>
+			<tr>
+				<th scope="col"> Lundi </th>
+				<th scope="col"> Mardi </th>
+				<th scope="col"> Mercredi </th>
+				<th scope="col"> Jeudi </th>
+				<th scope="col"> Vendredi </th>
+			</tr>
+		</thead>
 
-			<tbody>
-				<tr> <!-- 14h -->
-					<td class="lundi"><?php afficherCours("lundi", "14", $course); ?></td>
-					<td class="mardi"> <?php afficherCours("mardi", "14", $course); ?> </td>
-					<td class="mercredi"> <?php afficherCours("mercredi", "14", $course); ?></td>
-					<td class="jeudi"> <?php afficherCours("jeudi", "14", $course); ?> </td>
-					<td class="vendredi"> <?php afficherCours("vendredi", "14", $course); ?> </td>
-				</tr>
+		<tbody>
+			<tr> <!-- 14h -->
+				<td class="lundi"><?php afficherCours("lundi", "14", $course); ?></td>
+				<td class="mardi"> <?php afficherCours("mardi", "14", $course); ?> </td>
+				<td class="mercredi"> <?php afficherCours("mercredi", "14", $course); ?></td>
+				<td class="jeudi"> <?php afficherCours("jeudi", "14", $course); ?> </td>
+				<td class="vendredi"> <?php afficherCours("vendredi", "14", $course); ?> </td>
+			</tr>
 
-				<tr> <!-- 15h -->
-					<td class="lundi"><?php afficherCours("lundi", "15", $course); ?></td>
-					<td class="mardi"> <?php afficherCours("mardi", "15", $course); ?> </td>
-					<td class="mercredi"> <?php afficherCours("mercredi", "15", $course); ?></td>
-					<td class="jeudi"> <?php afficherCours("jeudi", "15", $course); ?> </td>
-					<td class="vendredi"> <?php afficherCours("vendredi", "15", $course); ?> </td>
-				</tr>
+			<tr> <!-- 15h -->
+				<td class="lundi"><?php afficherCours("lundi", "15", $course); ?></td>
+				<td class="mardi"> <?php afficherCours("mardi", "15", $course); ?> </td>
+				<td class="mercredi"> <?php afficherCours("mercredi", "15", $course); ?></td>
+				<td class="jeudi"> <?php afficherCours("jeudi", "15", $course); ?> </td>
+				<td class="vendredi"> <?php afficherCours("vendredi", "15", $course); ?> </td>
+			</tr>
 
-				<tr> <!-- 16h -->
-					<td class="lundi"><?php afficherCours("lundi", "16", $course); ?></td>
-					<td class="mardi"> <?php afficherCours("mardi", "16", $course); ?> </td>
-					<td class="mercredi"> <?php afficherCours("mercredi", "16", $course); ?></td>
-					<td class="jeudi"> <?php afficherCours("jeudi", "16", $course); ?> </td>
-					<td class="vendredi"> <?php afficherCours("vendredi", "16", $course); ?> </td>
-				</tr>
+			<tr> <!-- 16h -->
+				<td class="lundi"><?php afficherCours("lundi", "16", $course); ?></td>
+				<td class="mardi"> <?php afficherCours("mardi", "16", $course); ?> </td>
+				<td class="mercredi"> <?php afficherCours("mercredi", "16", $course); ?></td>
+				<td class="jeudi"> <?php afficherCours("jeudi", "16", $course); ?> </td>
+				<td class="vendredi"> <?php afficherCours("vendredi", "16", $course); ?> </td>
+			</tr>
 
-				<tr> <!-- 17h -->
-					<td class="lundi"><?php afficherCours("lundi", "17", $course); ?></td>
-					<td class="mardi"> <?php afficherCours("mardi", "17", $course); ?> </td>
-					<td class="mercredi"> <?php afficherCours("mercredi", "17", $course); ?></td>
-					<td class="jeudi"> <?php afficherCours("jeudi", "17", $course); ?> </td>
-					<td class="vendredi"> <?php afficherCours("vendredi", "17", $course); ?> </td>
-				</tr>
+			<tr> <!-- 17h -->
+				<td class="lundi"><?php afficherCours("lundi", "17", $course); ?></td>
+				<td class="mardi"> <?php afficherCours("mardi", "17", $course); ?> </td>
+				<td class="mercredi"> <?php afficherCours("mercredi", "17", $course); ?></td>
+				<td class="jeudi"> <?php afficherCours("jeudi", "17", $course); ?> </td>
+				<td class="vendredi"> <?php afficherCours("vendredi", "17", $course); ?> </td>
+			</tr>
 
-				<tr> <!-- 18h -->
-					<td class="lundi"><?php afficherCours("lundi", "18", $course); ?></td>
-					<td class="mardi"> <?php afficherCours("mardi", "18", $course); ?> </td>
-					<td class="mercredi"> <?php afficherCours("mercredi", "18", $course); ?></td>
-					<td class="jeudi"> <?php afficherCours("jeudi", "18", $course); ?> </td>
-					<td class="vendredi"> <?php afficherCours("vendredi", "18", $course); ?> </td>
-				</tr>
+			<tr> <!-- 18h -->
+				<td class="lundi"><?php afficherCours("lundi", "18", $course); ?></td>
+				<td class="mardi"> <?php afficherCours("mardi", "18", $course); ?> </td>
+				<td class="mercredi"> <?php afficherCours("mercredi", "18", $course); ?></td>
+				<td class="jeudi"> <?php afficherCours("jeudi", "18", $course); ?> </td>
+				<td class="vendredi"> <?php afficherCours("vendredi", "18", $course); ?> </td>
+			</tr>
 
-				<tr> <!-- 19h -->
-					<td class="lundi"><?php afficherCours("lundi", "19", $course); ?></td>
-					<td class="mardi"> <?php afficherCours("mardi", "19", $course); ?> </td>
-					<td class="mercredi"> <?php afficherCours("mercredi", "19", $course); ?></td>
-					<td class="jeudi"> <?php afficherCours("jeudi", "19", $course); ?> </td>
-					<td class="vendredi"> <?php afficherCours("vendredi", "19", $course); ?> </td>
-				</tr>
+			<tr> <!-- 19h -->
+				<td class="lundi"><?php afficherCours("lundi", "19", $course); ?></td>
+				<td class="mardi"> <?php afficherCours("mardi", "19", $course); ?> </td>
+				<td class="mercredi"> <?php afficherCours("mercredi", "19", $course); ?></td>
+				<td class="jeudi"> <?php afficherCours("jeudi", "19", $course); ?> </td>
+				<td class="vendredi"> <?php afficherCours("vendredi", "19", $course); ?> </td>
+			</tr>
 
-				<tr> <!-- 20h -->
-					<td class="lundi"><?php afficherCours("lundi", "20", $course); ?></td>
-					<td class="mardi"> <?php afficherCours("mardi", "20", $course); ?> </td>
-					<td class="mercredi"> <?php afficherCours("mercredi", "20", $course); ?></td>
-					<td class="jeudi"> <?php afficherCours("jeudi", "20", $course); ?> </td>
-					<td class="vendredi"> <?php afficherCours("vendredi", "20", $course); ?> </td>
-				</tr>
+			<tr> <!-- 20h -->
+				<td class="lundi"><?php afficherCours("lundi", "20", $course); ?></td>
+				<td class="mardi"> <?php afficherCours("mardi", "20", $course); ?> </td>
+				<td class="mercredi"> <?php afficherCours("mercredi", "20", $course); ?></td>
+				<td class="jeudi"> <?php afficherCours("jeudi", "20", $course); ?> </td>
+				<td class="vendredi"> <?php afficherCours("vendredi", "20", $course); ?> </td>
+			</tr>
 
-				<tr> <!-- 21h -->
-					<td class="lundi"><?php afficherCours("lundi", "21", $course); ?></td>
-					<td class="mardi"> <?php afficherCours("mardi", "21", $course); ?> </td>
-					<td class="mercredi"> <?php afficherCours("mercredi", "21", $course); ?></td>
-					<td class="jeudi"> <?php afficherCours("jeudi", "21", $course); ?> </td>
-					<td class="vendredi"> <?php afficherCours("vendredi", "21", $course); ?> </td>
-				</tr>
+			<tr> <!-- 21h -->
+				<td class="lundi"><?php afficherCours("lundi", "21", $course); ?></td>
+				<td class="mardi"> <?php afficherCours("mardi", "21", $course); ?> </td>
+				<td class="mercredi"> <?php afficherCours("mercredi", "21", $course); ?></td>
+				<td class="jeudi"> <?php afficherCours("jeudi", "21", $course); ?> </td>
+				<td class="vendredi"> <?php afficherCours("vendredi", "21", $course); ?> </td>
+			</tr>
 
-				<tr> <!-- 22h -->
-					<td class="lundi"><?php afficherCours("lundi", "22", $course); ?></td>
-					<td class="mardi"> <?php afficherCours("mardi", "22", $course); ?> </td>
-					<td class="mercredi"> <?php afficherCours("mercredi", "22", $course); ?></td>
-					<td class="jeudi"> <?php afficherCours("jeudi", "22", $course); ?> </td>
-					<td class="vendredi"> <?php afficherCours("vendredi", "22", $course); ?> </td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
+			<tr> <!-- 22h -->
+				<td class="lundi"><?php afficherCours("lundi", "22", $course); ?></td>
+				<td class="mardi"> <?php afficherCours("mardi", "22", $course); ?> </td>
+				<td class="mercredi"> <?php afficherCours("mercredi", "22", $course); ?></td>
+				<td class="jeudi"> <?php afficherCours("jeudi", "22", $course); ?> </td>
+				<td class="vendredi"> <?php afficherCours("vendredi", "22", $course); ?> </td>
+			</tr>
+		</tbody>
+	</table>
 </div>
-<a href="#"> Ajouter un cours</a>
+
+
+<p class="center-align"><a href="#modal_cours" class="modal-trigger" rel="modal:open"> Ajouter un cours </a></p>
+
+<div id="modal_cours" class="modal">
+  <h1> Ajouter un cours </h1>
+  <div class="modal_flex">
+    <form class="add_course"  method="post">
+    	<input type="text" name="" value="" placeholder="Nom du cours">
+			<input type="text" name="" value="" placeholder="Jour de la semaine">
+			<input type="text" name="" value="" placeholder="Horaires">
+			<input type="text" name="" value="" placeholder="Lieu">
+			<textarea name="name" rows="8" cols="80"> Description du cours </textarea>
+    </form>
+
+    <p class="p_modal">Pour bénéficier d'un tarif réduit, veuillez nous transmettre les justificatifs adéquats. <br>
+      Votre inscription ne sera valide qu'après leur vérification.
+    </p>
+  </div>
+</div>
