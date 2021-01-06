7f80abac3a5daa281698ad8ee9f51e3582137429
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2021-01-06 21:41:49 +0100

    affichage historique d'achats

---
+++ views/members/historique-achats.php
@@ -0,0 +1,32 @@
+<?php include('profil_nav.php'); ?>
+
+<h2 class="h2_compte"> Mes paiements </h2>
+
+<p class="center-align"> Historique des paiements effectués. </p>
+
+<div class="row">
+  <div class="col m10 s10 offset-m1">
+
+  <table class="centered striped">
+    <thead>
+      <tr>
+        <th> </th>
+        <th>Cours</th>
+        <th>Date</th>
+        <th>Montant payé</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <?php foreach($historique as $data){ ?>
+      <tr>
+        <td></td>
+        <td><?= $data['type_dance'] ?></td>
+        <td><?= $data['year'] ?></td>
+        <td> <?= $data['price'] . " euros" ?></td>
+      </tr>
+        <?php } ?>
+    </tbody>
+  </table>
+  </div>
+</div>
