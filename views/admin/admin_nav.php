2586b2f1f5fdbb44d17ecb713ff3a869b4e06e07
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2021-01-07 19:23:46 +0100

    barre de navigation admin

---
+++ views/admin/admin_nav.php
@@ -0,0 +1,7 @@
+<ul class="liste_compte">
+  <li><a class="a_compte" href="gestionDemandes"> Les demandes de participation aux cours </a></li>
+  <li><a class="a_compte" href="gestionDocument"> Les documents reçus </a></li>
+  <li><a class="a_compte" href="gestionMembres"> Les membres </a></li>
+  <li><a class="a_compte" href="creerNouveauCours"> Créer un nouveau cours </a></li>
+  <li><a class="a_compte" href="gestionFormules"> Les formules </a></li>
+</ul>
