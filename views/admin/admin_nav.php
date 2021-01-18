998acbf11be7e0f1c48e162344b336ad87c77c67
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2021-01-18 15:35:13 +0100

    Changement de page :sommaire menant aux demandes d'inscription aux cours

--- views/admin/admin_nav.php
+++ views/admin/admin_nav.php
@@ -1,5 +1,5 @@
 <ul class="liste_compte">
-  <li><a class="a_compte" href="<?= URL ?>administration/gestionDemandes"> Les demandes de participation aux cours </a></li>
+  <li><a class="a_compte" href="<?= URL ?>administration/listeCours"> Les demandes de participation aux cours </a></li>
   <li><a class="a_compte" href="<?= URL ?>administration/gestionDocument"> Les documents reçus </a></li>
   <li><a class="a_compte" href="<?= URL ?>administration/gestionMembres"> Les membres </a></li>
   <li><a class="a_compte" href="<?= URL ?>administration/creerNouveauCours"> Créer un nouveau cours </a></li>
