cb71696bd5b5867a01e00a76ee0c15499421f4cd
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2021-01-21 17:57:18 +0100

    BDD mise à jour
    Ajout d'une table types_courses permettant d'assigner une couleur à un type de cours en fonction d'un type de danse et d'un niveau.

--- cocoswing.sql
+++ cocoswing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3308
--- Généré le :  mar. 05 jan. 2021 à 15:40
+-- Généré le :  jeu. 21 jan. 2021 à 16:54
 -- Version du serveur :  8.0.18
 -- Version de PHP :  7.3.12
 
@@ -70,21 +70,22 @@ CREATE TABLE IF NOT EXISTS `courses` (
   `type_dance` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `profs` varchar(255) NOT NULL DEFAULT 'Les profs',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `courses`
 --
 
-INSERT INTO `courses` (`id`, `day`, `start_time`, `end_time`, `level`, `type_dance`, `address`, `description`) VALUES
-(1, 'mercredi', '18:45:00', '20:00:00', '1', 'solo', '', ''),
-(2, 'lundi', '18:45:00', '19:45:00', '2', 'solo', NULL, ''),
-(3, 'mardi', '21:30:00', '22:30:00', '1', 'lindy_hop', 'adresse', ''),
-(5, 'jeudi', '19:45:00', '21:00:00', '2', 'lindy_hop', NULL, ''),
-(6, 'lundi', '19:45:00', '21:00:00', '3', 'lindy_hop', NULL, ''),
-(7, 'lundi', '18:00:00', '19:00:00', '1', 'solo', 'adresse', 'description'),
-(9, 'vendredi', '20:00:00', '21:00:00', '3', 'lindy_hop', 'test', 'test');
+INSERT INTO `courses` (`id`, `day`, `start_time`, `end_time`, `level`, `type_dance`, `address`, `description`, `profs`) VALUES
+(1, 'mercredi', '18:45:00', '20:00:00', '1', 'solo', '', '', 'Les profs'),
+(2, 'lundi', '18:45:00', '19:45:00', '2', 'solo', NULL, '', 'Les profs'),
+(3, 'mardi', '21:30:00', '22:30:00', '1', 'lindy_hop', 'adresse', '', 'Les profs'),
+(5, 'jeudi', '19:45:00', '21:00:00', '2', 'lindy_hop', NULL, '', 'Les profs'),
+(6, 'lundi', '19:45:00', '21:00:00', '3', 'lindy_hop', NULL, '', 'Les profs'),
+(7, 'lundi', '18:00:00', '19:00:00', '1', 'solo', 'adresse', 'description', 'Les profs'),
+(9, 'vendredi', '20:00:00', '21:00:00', '3', 'lindy_hop', 'test', 'test', 'Les profs');
 
 -- --------------------------------------------------------
 
@@ -97,17 +98,22 @@ CREATE TABLE IF NOT EXISTS `courses_requests` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_course` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'attente',
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'waiting',
   `role_dance` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'indifferent',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `courses_requests`
 --
 
 INSERT INTO `courses_requests` (`id`, `id_course`, `id_user`, `status`, `role_dance`) VALUES
-(1, 5, 1, 'waiting', 'indifferent');
+(1, 1, 1, 'accepted', 'leader'),
+(2, 7, 1, 'waiting', 'leader'),
+(3, 6, 1, 'waiting', 'follower'),
+(4, 9, 1, 'waiting', 'indifferent'),
+(5, 5, 1, 'accepted', 'follower'),
+(6, 3, 1, 'accepted', 'indifferent');
 
 -- --------------------------------------------------------
 
@@ -145,47 +151,24 @@ INSERT INTO `errors` (`id`, `type_error`, `message_error`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `manage_courses_requests`
+-- Structure de la table `files`
 --
 
-DROP TABLE IF EXISTS `manage_courses_requests`;
-CREATE TABLE IF NOT EXISTS `manage_courses_requests` (
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE IF NOT EXISTS `files` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_course_request` int(11) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `manage_documents`
---
-
-DROP TABLE IF EXISTS `manage_documents`;
-CREATE TABLE IF NOT EXISTS `manage_documents` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `document` varchar(255) NOT NULL,
+  `filename` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `id_user` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'waiting',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Structure de la table `purchase_history`
+-- Déchargement des données de la table `files`
 --
 
-DROP TABLE IF EXISTS `purchase_history`;
-CREATE TABLE IF NOT EXISTS `purchase_history` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name_course` varchar(255) NOT NULL,
-  `teachers` varchar(255) NOT NULL,
-  `date` date NOT NULL,
-  `price` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `files` (`id`, `filename`, `id_user`, `status`) VALUES
+(1, 'css_liste.jpg', 3, 'denied');
 
 -- --------------------------------------------------------
 
@@ -200,6 +183,7 @@ CREATE TABLE IF NOT EXISTS `subscriptions` (
   `lower_price` tinyint(1) NOT NULL,
   `installment_payment` tinyint(1) NOT NULL,
   `price` int(11) NOT NULL,
+  `year` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `helloasso_link` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -208,20 +192,45 @@ CREATE TABLE IF NOT EXISTS `subscriptions` (
 -- Déchargement des données de la table `subscriptions`
 --
 
-INSERT INTO `subscriptions` (`id`, `type_dance`, `lower_price`, `installment_payment`, `price`, `helloasso_link`) VALUES
-(1, '1lindy', 0, 0, 325, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-de-lindy-hop-t-p-adhesion'),
-(2, '1lindy', 0, 1, 325, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/plein-1-cours-3x-sans-frais'),
-(3, '1lindy', 1, 0, 275, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-lindy-hop-t-r-adhesion-obligatoire-1'),
-(4, '1lindy', 1, 1, 275, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-lindy-hop-t-r-adhesion-obligatoire'),
-(5, '1solo', 0, 0, 280, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-solo-t-p-adhesion-obligatoire-en-3x-sans-frais'),
-(6, '1solo', 0, 1, 280, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-solo-t-p-adhesion-obligatoire-en-3x-sans-frais-1'),
-(7, '1solo', 1, 0, 250, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-solo-t-r-adhesion-obligatoire'),
-(8, '1solo', 1, 1, 250, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-solo-t-r-adhesion-obligatoire-en-3x-sans-frais'),
-(9, '1lindy_1solo', 0, 0, 485, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/2-cours-lindy-solo-t-p-adhesion-obligatoire'),
-(10, '1lindy_1solo', 0, 1, 485, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/2-cours-lindy-solo-t-p-adhesion-obligatoire-en-3x-sans-frais'),
-(11, '1lindy_1solo', 1, 0, 435, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/2-cours-lindy-solo-t-r-adhesion-obligatoire-en-3x-sans-frais-2'),
-(12, '1lindy_1solo', 1, 1, 435, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/2-cours-lindy-solo-t-r-adhesion-obligatoire-en-3x-sans-frais'),
-(13, '2lindy', 0, 1, 514, 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/2-cours-de-lindy-hop-t-p-adhesion-en-3x-sans-frais');
+INSERT INTO `subscriptions` (`id`, `type_dance`, `lower_price`, `installment_payment`, `price`, `year`, `helloasso_link`) VALUES
+(1, '1lindy', 0, 0, 325, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-de-lindy-hop-t-p-adhesion'),
+(2, '1lindy', 0, 1, 325, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/plein-1-cours-3x-sans-frais'),
+(3, '1lindy', 1, 0, 275, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-lindy-hop-t-r-adhesion-obligatoire-1'),
+(4, '1lindy', 1, 1, 275, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-lindy-hop-t-r-adhesion-obligatoire'),
+(5, '1solo', 0, 0, 280, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-solo-t-p-adhesion-obligatoire-en-3x-sans-frais'),
+(6, '1solo', 0, 1, 280, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-solo-t-p-adhesion-obligatoire-en-3x-sans-frais-1'),
+(7, '1solo', 1, 0, 250, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-solo-t-r-adhesion-obligatoire'),
+(8, '1solo', 1, 1, 250, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/1-cours-solo-t-r-adhesion-obligatoire-en-3x-sans-frais'),
+(9, '1lindy_1solo', 0, 0, 485, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/2-cours-lindy-solo-t-p-adhesion-obligatoire'),
+(10, '1lindy_1solo', 0, 1, 485, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/2-cours-lindy-solo-t-p-adhesion-obligatoire-en-3x-sans-frais'),
+(11, '1lindy_1solo', 1, 0, 435, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/2-cours-lindy-solo-t-r-adhesion-obligatoire-en-3x-sans-frais-2'),
+(12, '1lindy_1solo', 1, 1, 435, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/2-cours-lindy-solo-t-r-adhesion-obligatoire-en-3x-sans-frais'),
+(13, '2lindy', 0, 1, 514, '2021-01-06 20:20:04', 'https://www.helloasso.com/associations/coco-swing-marseille/adhesions/2-cours-de-lindy-hop-t-p-adhesion-en-3x-sans-frais');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `types_courses`
+--
+
+DROP TABLE IF EXISTS `types_courses`;
+CREATE TABLE IF NOT EXISTS `types_courses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name_level` varchar(255) NOT NULL,
+  `color` varchar(255) NOT NULL DEFAULT '#9CC1D9',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `types_courses`
+--
+
+INSERT INTO `types_courses` (`id`, `name_level`, `color`) VALUES
+(1, 'solo1', '#24BBC5'),
+(2, 'solo2', '#24BBC5'),
+(3, 'lindy1', '#FFD02E'),
+(4, 'lindy2', '#8FC552'),
+(5, 'lindy3', '#ED823A');
 
 -- --------------------------------------------------------
 
@@ -243,14 +252,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'default.jpg',
   `member` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `first_name`, `family_name`, `phone`, `pseudo_facebook`, `password`, `admin`, `registration_date`, `picture`, `member`) VALUES
-(1, 'test', 'testeur', 'testeur', 0, 'pseudo facebook', '$2y$10$1hHCfYtWPRoHKQ8xYnTfDejJNNjlITNIFKh40OiZ.aclTKiMMKQPe', 0, '2020-12-22 15:09:49', 'default.jpg', 0);
+(1, 'test', 'testeur', 'testeur', 0, 'pseudo facebook', '$2y$10$1hHCfYtWPRoHKQ8xYnTfDejJNNjlITNIFKh40OiZ.aclTKiMMKQPe', 0, '2020-12-22 15:09:49', 'default.jpg', 1),
+(3, 'admin', 'a', 'a', 111, 'a', '$2y$10$6wt1cIPrUazqB6Vo/nvMa.YmhTU5uguqFEIqdl7S2hABZCHMXj0T6', 1, '2021-01-19 09:41:55', 'default.jpg', 0);
 
 -- --------------------------------------------------------
 
@@ -278,7 +288,7 @@ CREATE TABLE IF NOT EXISTS `users_subscriptions` (
   `id_user` int(11) NOT NULL,
   `id_subscription` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `users_subscriptions`
@@ -293,7 +303,8 @@ INSERT INTO `users_subscriptions` (`id`, `id_user`, `id_subscription`) VALUES
 (6, 1, 6),
 (7, 1, 6),
 (8, 1, 2),
-(9, 1, 11);
+(9, 1, 11),
+(10, 1, 5);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
