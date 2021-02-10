5e22f5329d1d4963982bce73835cddea1e608d75
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2021-02-10 11:43:43 +0100

    div container materialize + correction name_planning vide

--- views/admin/liste-cours.php
+++ views/admin/liste-cours.php
@@ -1,3 +1,4 @@
+<div class="container">
 <?php include('admin_nav.php'); ?>
 
 <h2 class="h2_compte"> Liste des cours </h2>
@@ -6,16 +7,15 @@
 <?php //var_dump($solo); ?>
 <?php //var_dump($lindy); ?>
 
-
-
 <p class="center-align">Cours Solo</p>
 <div class="row center-align">
+  <div class="col s12 m12 ">
   <?php   for($i = 0; $i < count($solo['solo']); $i++) { ?>
-  <div class="col s12 m2">
+  <div class="col m3">
     <div class="card-panel teal waves-effect waves-block waves-light hoverable">
       <span class="white-text">
         <a class="white-text" href="gestionDemandes/<?= $solo['solo'][$i]['id'] ?>">
-          <?= $solo['solo'][$i]['type_dance'] . " " .  $solo['solo'][$i]['level'] ." " . $solo['solo'][$i]['day'] ?>
+          <?= $solo['solo'][$i]['name_planning'] . " <br> " . $solo['solo'][$i]['day'] ?>
         </a>
       </span>
     </div>
@@ -24,15 +24,17 @@
     }
     ?>
 </div>
+  </div>
 
 <p class="center-align">Cours Lindy Hop</p>
 <div class="row center-align">
+  <div class="col s12 m12">
   <?php for($i = 0; $i < count($lindy['lindy']); $i++) { ?>
-  <div class="col s12 m2">
+  <div class="col m3">
     <div class="card-panel teal waves-effect waves-block waves-light hoverable">
       <span class="white-text">
         <a class="white-text" href="gestionDemandes/<?= $lindy['lindy'][$i]['id'] ?>">
-          <?= $lindy['lindy'][$i]['type_dance'] . " " .  $lindy['lindy'][$i]['level'] ." " . $lindy['lindy'][$i]['day'] ?>
+          <?= $lindy['lindy'][$i]['name_planning'] . "<br>" . $lindy['lindy'][$i]['day'] ?>
         </a>
       </span>
     </div>
@@ -40,4 +42,6 @@
     <?php
     }
     ?>
+</div>
+</div>
 </div>
