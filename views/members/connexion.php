82bfe06e5433d157ed88ebd64aae154c5a0df396
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2021-02-16 14:00:29 +0100

    container + name dans input

--- views/members/connexion.php
+++ views/members/connexion.php
@@ -1,18 +1,20 @@
-<form class="form_connexion" method="post">
-  <div class="label_input">
-    <label for="email">Adresse e-mail </label>
-    <input type="text" name="email" id="email" placeholder="Saisir son adresse e-mail" required>
-  </div>
+<div class="container">
+  <form class="form_connexion" method="post">
+    <div class="label_input">
+      <label for="email">Adresse e-mail </label>
+      <input type="text" name="email" id="email" placeholder="Saisir son adresse e-mail" required>
+    </div>
 
-  <div class="label_input">
-    <label for="password">Mot de passe</label>
-    <input type="password" id="password" name="password" placeholder="Saisir son mot de passe" required>
-  </div>
+    <div class="label_input">
+      <label for="password">Mot de passe</label>
+      <input type="password" id="password" name="password" placeholder="Saisir son mot de passe" required>
+    </div>
 
-  <div class="center">
-    <button type="submit" class="button_pages"> Connexion </button>
-    <p class="p_inscription">
-      Nouveau ? <a class="link_inscription" href="<?= URL ?>members/inscription">Cliquez ici</a> pour vous inscrire.
-    </p>
-  </div>
-</form>
+    <div class="center">
+      <button type="submit" class="button_pages"> Connexion </button>
+      <p>
+        Nouveau ? <a class="link_inscription" href="<?= URL ?>members/inscription">Cliquez ici</a> pour vous inscrire.
+      </p>
+    </div>
+  </form>
+</div>
--- views/members/inscription.php
+++ views/members/inscription.php
@@ -1,47 +1,52 @@
-<div class="erreur hidden"></div>
-<form class="form_connexion" action="inscription" method="post" id="inscription">
-
-  <div class="label_input">
-    <label for="email">Adresse e-mail </label>
-    <input type="text" name="email" id="email" placeholder="Saisir son adresse e-mail">
-  </div>
-
-  <div class="label_input">
-    <label for="first_name"> Prénom </label>
-    <input type="text" name="first_name" id="first_name" placeholder="Saisir son prénom">
-  </div>
-
-  <div class="label_input">
-    <label for="family_name"> Nom </label>
-    <input type="text" name="family_name" id="family_name" placeholder="Saisir son nom">
-  </div>
-
-  <div class="label_input">
-    <label for="phone"> N° de téléphone </label>
-    <input type="tel" name="phone" id="phone" placeholder="Saisir son numéro de téléphone">
-  </div>
-
-  <div class="label_input">
-    <label for="pseudo_facebook"> Pseudo Facebook* </label>
-    <input type="text" name="pseudo_facebook" id="pseudo_facebook" placeholder="Saisir son pseudo Facebook">
-  </div>
-
-  <div class="label_input">
-    <label for="password">Mot de passe (8 caractères minimum)</label>
-    <input type="password" id="password" name="password"
-    placeholder="Saisir son mot de passe">
-  </div>
-
-  <div class="label_input">
-    <label for="confirm_password">Confirmation du mot de passe </label>
-    <input type="password" id="confirm_password" name="confirm_password"
-    placeholder="Confirmer son mot de passe">
-  </div>
-
-  <button type="submit" class="button_pages" name="submit"> Inscription </button>
-
-  <p class="p_facultatif">
-    * Champ facultatif. <br>
-    Cela nous permettrait de te retrouver et de t'inviter dans les groupes liés aux activités de l'association.
+<div class="container">
+  <div class="erreur hidden"></div>
+  <p class="center">
+    Vous avez déjà un compte ? <a class="link_inscription" href="<?= URL ?>members/inscription">Cliquez ici</a> pour vous connecter.
   </p>
-</form>
+  <form class="form_connexion" action="inscription" method="post" id="inscription">
+
+    <div class="label_input">
+      <label for="email">Adresse e-mail </label>
+      <input type="text" name="email" id="email" placeholder="Saisir son adresse e-mail">
+    </div>
+
+    <div class="label_input">
+      <label for="first_name"> Prénom </label>
+      <input type="text" name="first_name" id="first_name" placeholder="Saisir son prénom">
+    </div>
+
+    <div class="label_input">
+      <label for="family_name"> Nom </label>
+      <input type="text" name="family_name" id="family_name" placeholder="Saisir son nom">
+    </div>
+
+    <div class="label_input">
+      <label for="phone"> N° de téléphone </label>
+      <input type="tel" name="phone" id="phone" placeholder="Saisir son numéro de téléphone">
+    </div>
+
+    <div class="label_input">
+      <label for="pseudo_facebook"> Pseudo Facebook* </label>
+      <input type="text" name="pseudo_facebook" id="pseudo_facebook" placeholder="Saisir son pseudo Facebook">
+    </div>
+
+    <div class="label_input">
+      <label for="password">Mot de passe (8 caractères minimum)</label>
+      <input type="password" id="password" name="password"
+      placeholder="Saisir son mot de passe">
+    </div>
+
+    <div class="label_input">
+      <label for="confirm_password">Confirmation du mot de passe </label>
+      <input type="password" id="confirm_password" name="confirm_password"
+      placeholder="Confirmer son mot de passe">
+    </div>
+
+    <button type="submit" class="button_pages" name="submit"> Inscription </button>
+
+    <p class="p_facultatif">
+      * Champ facultatif. <br>
+      Cela nous permettrait de te retrouver et de t'inviter dans les groupes liés aux activités de l'association.
+    </p>
+  </form>
+</div>
