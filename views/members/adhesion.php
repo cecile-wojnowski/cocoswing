d8ea491ddef438d92301ab53b035c01ee0ac8185
Cécile <62134503+cecile-wojnowski@users.noreply.github.com>
2021-02-17 14:32:11 +0100

    formulaire réagencé

--- views/members/adhesion.php
+++ views/members/adhesion.php
@@ -1,90 +1,100 @@
 <?php include('profil_nav.php'); ?>
 
+<div class="container">
+
+
 <?php if(isset($helloAsso)){ ?>
 <p class="center-align"> <a href="<?php echo $helloAsso; ?>">Cliquez ici pour adhérer (formulaire HelloAsso) </a><p>
 <?php }else{ ?>
 
 <h2 class="center-align h2_compte">Choix de cours annuels</h2>
-<div class="row">
 
-  <form action="adhesion" method="post" class="center-align">
-    <p class="center-align p_cours">Choisir un cours</p>
-    <p>
-      <label>
-        <input name="type_dance" value="1solo" type="radio" />
-        <span>1 cours de Solo</span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <input name="type_dance" value="1lindy" type="radio" />
-        <span>1 cours de Lindy Hop</span>
-      </label>
-    </p>
-    <p class="center-align p_cours">Autres choix</p>
-    <p>
-      <label>
-        <input name="type_dance" value="1lindy_1solo" type="radio"  />
-        <span> 1 cours de Lindy + 1 cours de Solo</span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <input name="type_dance" value="2lindy" type="radio"  />
-        <span> 2 cours de Lindy</span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <input name="type_dance" type="radio" disabled="disabled" />
-        <span> 2 cours de Lindy + 1 cours de solo (disponible bientôt)</span>
-      </label>
-    </p>
 
-    <p class="center-align p_cours">Réductions</p>
-    <p>
-      <label>
-        <input name="lower_price" value="0" type="radio" checked />
-        <span> Sans réduction </span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <input name="lower_price" value="1" type="radio"  />
-        <span> Tarif étudiant </span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <input name="lower_price" value="1" type="radio"  />
-        <span> Bénéficiaire du RSA</span>
-      </label>
-    </p>
+  <form action="adhesion" method="post">
+    <div class="row">
+      <div class="col s4 m4">
+        <p class="center-align p_cours">Choisir un cours</p>
+        <p>
+          <label>
+            <input name="type_dance" value="1solo" type="radio" />
+            <span>1 cours de Solo</span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <input name="type_dance" value="1lindy" type="radio" />
+            <span>1 cours de Lindy Hop</span>
+          </label>
+        </p>
+        <p class="center-align p_cours">Autres choix</p>
+        <p>
+          <label>
+            <input name="type_dance" value="1lindy_1solo" type="radio"  />
+            <span> 1 cours de Lindy + 1 cours de Solo</span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <input name="type_dance" value="2lindy" type="radio"  />
+            <span> 2 cours de Lindy</span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <input name="type_dance" type="radio" disabled="disabled" />
+            <span> 2 cours de Lindy + 1 cours de solo (disponible bientôt)</span>
+          </label>
+        </p>
+      </div>
 
-    <p class="center-align p_cours">Payer en plusieurs fois</p>
-    <p>
-      <label>
-        <input name="installment_payment" value="0" type="radio"  />
-        <span> En 1x </span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <input name="installment_payment" value="1" type="radio"  />
-        <span> En 3x sans frais </span>
-      </label>
-    </p>
-
-    <button type="submit" name="button">Adhérer</button>
-
+      <div class="col s4 m4">
+        <p class="center-align p_cours">Réductions</p>
+        <p>
+          <label>
+            <input name="lower_price" value="0" type="radio" checked />
+            <span> Sans réduction </span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <input name="lower_price" value="1" type="radio"  />
+            <span> Tarif étudiant </span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <input name="lower_price" value="1" type="radio"  />
+            <span> Bénéficiaire du RSA</span>
+          </label>
+        </p>
+      </div>
+      <div class="col s4 m4">
+        <p class="center-align p_cours">Payer en plusieurs fois</p>
+        <p>
+          <label>
+            <input name="installment_payment" value="0" type="radio"  />
+            <span> En 1x </span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <input name="installment_payment" value="1" type="radio"  />
+            <span> En 3x sans frais </span>
+          </label>
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <button type="submit" name="button">Adhérer</button>
+    </div>
   </form>
-</div>
+
 
 <?php } ?>
 
-<button type="button" name="" class="button_pages" id="btn_adhesion">
-  <a href="#modal_justificatif" class="modal-trigger" rel="modal:open"> Afficher modal </a>
-</button>
+
+  <a href="#modal_justificatif" class="modal-trigger" rel="modal:open"> Justificatif </a>
+
 <div id="modal_justificatif" class="modal">
   <h1> Demande de justificatif </h1>
   <div class="flex-row p-10">
@@ -101,4 +111,5 @@
       Votre inscription ne sera valide qu'après leur vérification.
     </p>
   </div>
+</div>
 </div>
