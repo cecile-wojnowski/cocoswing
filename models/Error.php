6e62b1e5a0a3938da9fea5b1973d20a734008dbb
Cécile Wojnowski <cecile.wojnowski@laplateforme.io>
2021-03-10 14:16:23 +0100

    verifierNombreEmail() + verifierInscription() + verifierConnexion()

--- models/Error.php
+++ models/Error.php
@@ -5,8 +5,114 @@ class Error extends Model{
   protected $_typeErreur;
   protected $_messageErreur;
 
-  public function __construct($_bdd){
-    return $this->_connection = $_bdd;
+  public function __construct(){
+    $this->table = "errors";
+    $this->getConnection();
+  }
+
+  /* Erreurs à ajouter :
+  - unknown email : comparer l'e-mail entré et ceux stockés en bdd
+  - wrong password
+  - format_phone : le numéro de tel ne doit contenir que des chiffres */
+
+  public function verifierNombreEmail(){
+    $getEmail = $bdd->prepare("SELECT COUNT(*) FROM utilisateurs WHERE email = ? ");
+    $getEmail->execute([$_POST['email']]);
+
+    $resultat = $getEmail->fetchColumn();
+
+    if($resultat == 0){
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  public function verifierPassword(){
+
+  }
+
+  public function verifierInscription(){
+
+    if(empty($_POST['email'])){
+      $empty = "empty_email";
+      $this->afficherErreur($empty);
+
+    }elseif(empty($_POST['name']))
+    {
+      $empty = "empty_name";
+      $this->afficherErreur($empty);
+
+    }elseif(empty($_POST['family_name']))
+    {
+      $empty = "empty_family_name";
+      $this->afficherErreur($empty);
+
+    }elseif(empty($_POST['password']))
+    {
+      $empty = "empty_password";
+      $this->afficherErreur($empty);
+
+    }elseif(empty($_POST['confirm_password']))
+    {
+      $empty = "empty_confirm";
+      $this->afficherErreur($empty);
+
+    }elseif(empty($_POST['email']))
+    {
+      $empty = "empty_email";
+      $this->afficherErreur($empty);
+
+    }elseif($_POST['password'] != $_POST['confirm_password']) // Vérifie que les deux mots de passe sont identiques
+    {
+      $error = "error_password";
+      $this->afficherErreur($error);
+
+    }elseif(strlen($_POST['password']) < 8) // Impose une taille minimale au password
+    {
+      $short = "short_password";
+      $this->afficherErreur($short);
+
+    }elseif(strlen($_POST['phone']) == 10)
+    {
+      $sizePhone = "size_phone";
+      $this->afficherErreur($sizePhone);
+
+    }elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+    {
+      $format = "format_email";
+      $this->afficherErreur($format);
+
+    }elseif($_POST['email'])
+    {
+      $email = $this->verifierNombreEmail($_POST['email']);
+
+      if($email = true){
+        $emailIndisponible = "email_not_available";
+        $this->afficherErreur($emailIndisponible);
+      }
+    }
+  }
+
+  public function verifierConnexion(){
+
+    if(empty($_POST['email'])){
+      $empty = "empty_email";
+      $this->afficherErreur($empty);
+
+    }elseif($_POST['email'])
+    {
+      $email = $this->verifierNombreEmail($_POST['email']);
+
+      if($email = false){
+        $emailInconnu = "unknown_email";
+        $this->afficherErreur($emailInconnu);
+      }
+    }elseif($_POST['password'] != $_POST['confirm_password'])
+    {
+      $error = "error_password";
+      $this->afficherErreur($error);
+    }
   }
 
   public function afficherErreur($typeErreur){
@@ -16,12 +122,5 @@ class Error extends Model{
 
     echo $resultat['message_erreur'];
   }
-  public function __construct()
-{
-    // Nous définissons la table par défaut de ce modèle
-    $this->table = "errors";
 
-    // Nous ouvrons la connexion à la base de données
-    $this->getConnection();
-}
 } ?>
