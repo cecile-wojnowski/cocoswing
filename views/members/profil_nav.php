671b1a2f8575466c7157cb6430cb9690b2520ae5
Cécile Wojnowski <cecile.wojnowski@laplateforme.io>
2021-03-12 15:37:03 +0100

    changement d'ordre des liens

--- views/members/profil_nav.php
+++ views/members/profil_nav.php
@@ -2,13 +2,14 @@
   header('Location:'.URL.'website/index');
 } ?>
 <ul class="liste_compte">
-  <li><a class="a_compte" href="<?= URL ?>members/monProfil"> Mes informations </a></li>
+
   <?php if($_SESSION['member'] == 0){ ?>
   <li><a class="a_compte" href="<?= URL ?>members/adhesion"> S'inscrire à un cours </a></li>
-<?php }elseif($_SESSION['member'] == 1){ ?>
+  <?php }elseif($_SESSION['member'] == 1){ ?>
   <li><a class="a_compte" href="<?= URL ?>members/videos"> Nos vidéos </a></li>
-<?php } ?>
+  <?php } ?>
   <li><a class="a_compte" href="<?= URL ?>members/planning"> Voir le planning </a></li>
+  <li><a class="a_compte" href="<?= URL ?>members/monProfil"> Mes informations </a></li>
   <li><a class="a_compte" href="<?= URL ?>members/demandesCours"> Mes demandes en attente </a></li>
   <li><a class="a_compte" href="<?= URL ?>members/historiqueAchats"> Mes paiements </a></li>
   <?php if($_SESSION['admin'] == 1){ ?>
