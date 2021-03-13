a4370b1fecead1ca419eca9fa1dc9d4ccfe1419c
Cécile Wojnowski <cecile.wojnowski@laplateforme.io>
2021-03-13 17:18:01 +0100

    CSS liens menant aux cours

--- views/website/cours.php
+++ views/website/cours.php
@@ -1,8 +1,8 @@
 <div class="container">
   <h1 class="center-align"> Cours & stages </h1>
   <div class="row">
-    <div class="col s4 m4">
-      <div class="section">
+
+      <div class="col m5 s5">
         <a href="<?= URL ?>website/coursReguliers#planning">
           <button class="z-depth-0 scale-cours1 scale-transition scale-out"> Planning </button>
         </a>
@@ -16,25 +16,8 @@
           <button class="z-depth-0 scale-cours1 scale-transition scale-out"> Lieux </button>
         </a>
       </div>
-      <button class="background-blue course_trigger circle z-depth-1" id="scale-cours-trigger1">
-        <a class="white-text" href="<?= URL ?>website/coursReguliers">Cours réguliers </a>
-      </button>
-    </div>
 
-    <div class="col s4 m4">
-      <div class="section">
-        <button class="z-depth-0 scale-cours2 scale-transition scale-out"> Planning </button>
-        <button class="z-depth-0 scale-cours2 scale-transition scale-out"> Niveaux </button>
-        <button class="z-depth-0 scale-cours2 scale-transition scale-out"> Tarifs </button>
-        <button class="z-depth-0 scale-cours2 scale-transition scale-out"> Lieux </button>
-      </div>
-      <button class="background-blue course_trigger circle z-depth-1" id="scale-cours-trigger2">
-        <a class="white-text" href="<?= URL ?>website/stages"> Stages </a>
-      </button>
-    </div>
-
-    <div class="col s4 m4">
-      <div class="section">
+      <div class="col offset m3 s3 m2">
         <a href="<?= URL ?>website/coursParticuliers#presentation">
           <button class="z-depth-0 scale-cours3 scale-transition scale-out"> Présentation </button>
         </a>
@@ -42,9 +25,34 @@
           <button class="z-depth-0 scale-cours3 scale-transition scale-out"> Contact </button>
         </a>
       </div>
-      <button class="background-blue course_trigger circle z-depth-1" id="scale-cours-trigger3">
-        <a class="white-text" href="<?= URL ?>website/coursParticuliers"> Cours particuliers </a>
-      </button>
+  </div>
+
+  <div class="row">
+    <a href="<?= URL ?>website/coursReguliers">
+      <button class="white-text background-blue course_trigger circle z-depth-1" id="scale-cours-trigger1"> Cours réguliers </button>
+    </a>
+
+    <a href="<?= URL ?>website/stages">
+      <button class="white-text background-blue course_trigger circle z-depth-1" id="scale-cours-trigger2"> Les <br> stages </button>
+    </a>
+
+    <a class="white-text" href="<?= URL ?>website/coursParticuliers">
+      <button class="white-text background-blue course_trigger circle z-depth-1" id="scale-cours-trigger3"> Cours particuliers </button>
+    </a>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   </div>
 </div>
